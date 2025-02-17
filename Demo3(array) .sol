50abd54aab2e8978b78edb0d78beb6c1c65d931f
egar6567 <egar6567@yahoo.com>
2025-02-17 12:54:43 +0600

    Create Demo3(array) .sol

---
+++ Demo3(array) .sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Demo {
+
+    uint256[10] public items = [1,2,3];
+    string[10] public word = ["hello", "in", "wordl"]; 
+    uint256[3][2] public items2;
+    uint256[] public items3;
+
+    function demo() public {
+        items[0] = 11;
+        items[1] = 10;
+        items[2] = 20;
+        items[5] = 30;
+    }
+
+    function demo2() public {
+        word[0] = "eg";
+        word[1] = "or";
+        word[2] = "nomber one";
+    }
+
+    function demo3() public {
+        items2 = [
+            [3,4,5],
+            [6,7,8]
+        ];
+    }
+
+    function demo4() public {
+        items3.push(1);
+        items3.push(6);
+        items3.push(3);
+    }
+}   
